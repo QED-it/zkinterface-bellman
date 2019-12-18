2631e956e5899a9be5e2f8cc1c8788818c7b0a46
aurel <aurel@qed-it.com>
2019-12-18 16:24:25 +0100

    Readme

---
+++ README.md
@@ -0,0 +1,9 @@
+# zkInterface Bellman adapter
+
+See https://github.com/QED-it/zkinterface
+
+## Example:
+
+    cat src/test/messages/circuit_r1cs.zkif src/test/messages/r1cs.zkif       | cargo run --release
+    cat src/test/messages/circuit_witness.zkif src/test/messages/witness.zkif | cargo run --release
+
--- src/bin.rs
+++ src/bin.rs
@@ -3,11 +3,7 @@ use std::io;
 use std::io::Read;
 use std::env;
 
-// Example:
-//
-//     cat src/test/circuit_r1cs.zkif src/test/r1cs.zkif       | cargo run --release
-//     cat src/test/circuit_witness.zkif src/test/witness.zkif | cargo run --release
-//
+
 pub fn main() -> Result<(), Box<std::error::Error>> {
     let mut messages = Messages::new(1);
 
